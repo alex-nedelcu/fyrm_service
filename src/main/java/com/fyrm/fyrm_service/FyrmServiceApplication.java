ea26f72ffa86755c9c7280617b8506cdbdb8a0ee
alex-nedelcu <alexandru.nedelcu01@yahoo.com>
2022-12-27 09:53:41 +0200

    Initialize Spring Boot project and create CI/CD yml file

---
+++ src/main/java/com/fyrm/fyrm_service/FyrmServiceApplication.java
@@ -0,0 +1,13 @@
+package com.fyrm.fyrm_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FyrmServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FyrmServiceApplication.class, args);
+	}
+
+}
