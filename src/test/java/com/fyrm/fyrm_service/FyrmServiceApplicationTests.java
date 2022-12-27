6748757ac8ea3b77bbee00405cb9d97d624e8f1b
alex-nedelcu <alexandru.nedelcu01@yahoo.com>
2022-12-27 14:41:45 +0200

    Should fail

--- src/test/java/com/fyrm/fyrm_service/FyrmServiceApplicationTests.java
+++ src/test/java/com/fyrm/fyrm_service/FyrmServiceApplicationTests.java
@@ -1,5 +1,7 @@
 package com.fyrm.fyrm_service;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,5 +10,11 @@ class FyrmServiceApplicationTests {
 
   @Test
   void contextLoads() {
+  }
+
+  @Test
+  void shouldPass() {
+    int a = 3;
+    assertEquals(a, 4);
   }
 }
