6f4541f664161047409b01d0df1afe9b8e135381
alex-nedelcu <alexandru.nedelcu01@yahoo.com>
2023-01-04 21:23:51 +0200

    Remove initial example entity

--- src/main/java/com/fyrm/fyrm_service/application/port/out/FindAccountPort.java
+++
@@ -1,9 +0,0 @@
-package com.fyrm.fyrm_service.application.port.out;
-
-import com.fyrm.fyrm_service.domain.Account;
-import java.util.List;
-
-public interface FindAccountPort {
-
-  List<Account> findAllByName(String name);
-}
