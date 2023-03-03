5ed1fe4aa3da8ebfb68842b5bc29b292610b7d1b
alex-nedelcu <alexandru.nedelcu01@yahoo.com>
2023-03-03 21:35:46 +0200

    looks good

--- src/main/java/com/fyrm/fyrm_service/adapters/out/persistence/entity/SearchProfileEntity.java
+++ src/main/java/com/fyrm/fyrm_service/adapters/out/persistence/entity/SearchProfileEntity.java
@@ -3,12 +3,15 @@ package com.fyrm.fyrm_service.adapters.out.persistence.entity;
 import com.fyrm.fyrm_service.adapters.out.persistence.entity.base.Identifiable;
 import com.fyrm.fyrm_service.infrastructure.spring.security.model.User;
 import java.util.List;
+import javax.persistence.CascadeType;
 import javax.persistence.CollectionTable;
 import javax.persistence.Column;
 import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.EntityListeners;
 import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
 import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import lombok.Data;
@@ -63,4 +66,12 @@ public class SearchProfileEntity extends Identifiable {
   @CollectionTable(name = "search_profile_bathroom_options", joinColumns = @JoinColumn(name = "search_profile_id"))
   @Column(name = "bathroom_options", nullable = false)
   private List<String> bathroomOptions;
+
+  @ManyToMany(cascade = CascadeType.ALL)
+  @JoinTable(
+      name = "used_search_profiles",
+      joinColumns = @JoinColumn(name = "search_profile_id"),
+      inverseJoinColumns = @JoinColumn(name = "rent_connection_id")
+  )
+  private List<RentConnectionEntity> rentConnections;
 }
