d3671dccb983386e7baea70f140541fcb6eb1347
alex-nedelcu <alexandru.nedelcu01@yahoo.com>
2023-03-03 21:36:52 +0200

    looks good

---
+++ src/main/java/com/fyrm/fyrm_service/adapters/out/persistence/entity/RentMateProposalEntity.java
@@ -0,0 +1,35 @@
+package com.fyrm.fyrm_service.adapters.out.persistence.entity;
+
+import com.fyrm.fyrm_service.adapters.out.persistence.entity.base.Identifiable;
+import java.util.List;
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EntityListeners;
+import javax.persistence.JoinColumn;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+import lombok.experimental.SuperBuilder;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+@Entity
+@Table(name = "rent_mate_proposals", schema = "public")
+@SuperBuilder
+@Data
+@EqualsAndHashCode(callSuper = true)
+@ToString(callSuper = true)
+@NoArgsConstructor
+@EntityListeners(AuditingEntityListener.class)
+public class RentMateProposalEntity extends Identifiable {
+
+  @Column(name = "rent_connection_id", nullable = false)
+  private Long rentConnectionId;
+
+  @OneToMany(cascade = CascadeType.ALL, orphanRemoval = true)
+  @JoinColumn(name = "rent_mate_proposal_id", nullable = false)
+  private List<ProposedRentMateEntity> proposedRentMates;
+}
