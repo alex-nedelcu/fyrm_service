4ed6474818b5b1e7e68f55ea1820e07259987c69
alex-nedelcu <alexandru.nedelcu01@yahoo.com>
2023-03-03 21:36:40 +0200

    looks good

---
+++ src/main/java/com/fyrm/fyrm_service/adapters/out/persistence/repository/RentMateProposalRepository.java
@@ -0,0 +1,10 @@
+package com.fyrm.fyrm_service.adapters.out.persistence.repository;
+
+import com.fyrm.fyrm_service.adapters.out.persistence.entity.RentMateProposalEntity;
+import com.fyrm.fyrm_service.infrastructure.persistence.BaseRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface RentMateProposalRepository extends BaseRepository<RentMateProposalEntity, Long> {
+
+}
