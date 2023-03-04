47160721ac550b63f69ba06362d0ca8546902614
alex-nedelcu <alexandru.nedelcu01@yahoo.com>
2023-03-04 15:16:59 +0200

    looks good

---
+++ src/main/java/com/fyrm/fyrm_service/application/port/out/FindRentMateProposalPort.java
@@ -0,0 +1,9 @@
+package com.fyrm.fyrm_service.application.port.out;
+
+import com.fyrm.fyrm_service.domain.RentMateProposal;
+import java.util.Optional;
+
+public interface FindRentMateProposalPort {
+
+  Optional<RentMateProposal> findByRentConnectionId(Long rentConnectionId);
+}
